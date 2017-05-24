73a7fb3e11bdf500497d3574ffb48a37f91b9e46
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-05-24 11:13:35 +0200

    Rename project to CottonRenderer

--- SoftEngineWPF/MainWindow.xaml.cs
+++ SoftEngineWPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SoftEngineWPF
+namespace CottonWPF
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
